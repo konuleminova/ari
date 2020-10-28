ecb7ffa160d5c814fadcff82d7d74c823968216d
konuleminova <konul.eminova39@gmail.com>
2020-10-28 11:44:35 +0400

    design part1

--- lib/ui/views/home/widgets/product_partner_item.dart
+++ lib/ui/views/home/widgets/product_partner_item.dart
@@ -11,30 +11,30 @@ class PartnerItem extends StatelessWidget {
     // TODO: implement build
     return Container(
       width: SizeConfig().screenWidth / 2,
-      padding: EdgeInsets.symmetric(horizontal: 4.toWidth),
-      child: Column(
+      padding: EdgeInsets.symmetric(horizontal: 3.toWidth),
+      child: Stack(
         children: <Widget>[
-          Expanded(
-            flex: 3,
+          Container(
+              child: ClipRRect(
+                  child: Image.network(
+                      'https://bees.az/___entcpanel/uploads/ef4534a27895456ef72f5acd7703ec9f_331778.png',
+                      fit: BoxFit.cover),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(4),
+                      topRight: Radius.circular(4)))),
+          Positioned(
+            bottom: 34.toHeight,left: 0,right: 0,
             child: Container(
-                child: ClipRRect(
-                    child: Image.network(
-                        'https://bees.az/___entcpanel/uploads/d3271c371aae25d4f8c747912391ce93_206431.png',
-                        fit: BoxFit.cover),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10)))),
-          ),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.all(8.toWidth),
+              padding: EdgeInsets.all(10.toHeight),
               width: SizeConfig().screenWidth,
                 alignment: index % 2 == 0?Alignment.centerRight:Alignment.centerLeft,
                 decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(bottomRight: Radius.circular(4),bottomLeft: Radius.circular(4)),
                     image: DecorationImage(
                   image:index % 2 == 0
-                      ? AssetImage('assets/images/green_clipper.png',)
-                      : AssetImage('assets/images/green_clipper_right.png'),
+                      ? AssetImage('assets/images/2-0.jpg',)
+                      : AssetImage('assets/images/3-0.jpg'),
+                      fit: BoxFit.cover
                 )),
                 child: Text('Cup Cup Coffie',textAlign: TextAlign.center,)),
           )
