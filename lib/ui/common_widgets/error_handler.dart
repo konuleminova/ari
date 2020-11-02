4e645315853564934c6c5695e5cf6239927c2678
konuleminova <konul.eminova39@gmail.com>
2020-11-02 13:10:30 +0400

    design food header design added sliver appbar

--- lib/ui/common_widgets/error_handler.dart
+++ lib/ui/common_widgets/error_handler.dart
@@ -1,45 +1,59 @@
 import 'package:ari/services/api_helper/api_response.dart';
 import 'package:ari/services/hooks/useSideEffect.dart';
 import 'package:ari/ui/common_widgets/loading.dart';
+import 'package:ari/utils/theme_color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ari/utils/size_config.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
 
 class CustomErrorHandler extends HookWidget {
   final List<Status> statuses;
   final List<AppException> errors;
   final Widget child;
 
-  const CustomErrorHandler({
-    this.statuses,
-    this.child,
-    this.errors,
-});
+  const CustomErrorHandler({this.statuses, this.child, this.errors});
 
   @override
   Widget build(BuildContext context) {
-    final AppException error = errors.firstWhere((element) => element != null, orElse: () => null);
+    final AppException error =
+        errors.firstWhere((element) => element != null, orElse: () => null);
     final bool hasError = error != null;
-    final bool isLoading = statuses.firstWhere((element) => element == Status.Loading, orElse: () => null) != null;
+    final bool isLoading = statuses.firstWhere(
+            (element) => element == Status.Loading,
+            orElse: () => null) !=
+        null;
     final ctx = useContext();
     useSideEffect(() {
-      if(hasError) {
+      if (hasError) {
         showDialog(
             context: ctx,
             builder: (BuildContext context) => ErrorDialog(
-              errorMessage: error.message ?? "Some Message",
-            ));
+                  errorMessage: error.message ?? "Some Message",
+                ));
       }
 
       return () {};
     }, [hasError, error]);
 
-    return hasError ? Container() : isLoading ? Loading() : child;
+    return hasError
+        ? Container(
+            child: InkWell(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.refresh),
+                    SizedBox(
+                      height: 8.toHeight,
+                    ),
+                    Text('Tap to refresh')
+                  ],
+                ),
+                onTap: () {}))
+        : isLoading ? Loading() : child;
   }
 }
-
 
 class ErrorDialog extends StatelessWidget {
   String errorMessage;
@@ -53,19 +67,24 @@ class ErrorDialog extends StatelessWidget {
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         child: Container(
           padding: EdgeInsets.all(16.toHeight),
-          height: 140.toHeight,
+          // height: 140.toHeight,
           decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
           child: Center(
               child: Column(
             children: <Widget>[
-              Text(errorMessage ?? 'Some Unkown Error Occured.'),
+              Expanded(
+                child: Text(errorMessage ?? 'Some Unkown Error Occured.'),
+              ),
               SizedBox(
                 height: 16.toHeight,
               ),
               Container(
                 margin: EdgeInsets.symmetric(horizontal: 16.toWidth),
+                height: 34.toHeight,
                 child: RaisedButton(
-                  color: Colors.green,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: ThemeColor().yellowColor,
                   child: Text('Ok'),
                   onPressed: () {
                     Navigator.pop(context);
