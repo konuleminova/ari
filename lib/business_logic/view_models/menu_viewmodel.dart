ed543cd8d1d41fd27684c9f8cbb363a7270bf1eb
konuleminova <konul.eminova39@gmail.com>
2020-11-04 14:25:49 +0400

    added resfresh

--- lib/business_logic/view_models/menu_viewmodel.dart
+++ lib/business_logic/view_models/menu_viewmodel.dart
@@ -13,6 +13,7 @@ import 'package:ari/utils/size_config.dart';
 class MenuViewModel extends HookWidget {
   final String id;
   final ScrollController scrollController;
+  ScrollController horizontalScrollController=new ScrollController();
   List<Food> foodList;
   int index = 0;
   ApiResponse<List<Menu>> menuList;
@@ -30,6 +31,7 @@ class MenuViewModel extends HookWidget {
     // TODO: implement build
     return CustomErrorHandler(
       child: ListView.builder(
+        controller: horizontalScrollController,
         itemBuilder: (BuildContext context, int index) {
           return InkWell(
             child: MenuItem(menu: apiResponse.value.data[index]),
@@ -42,10 +44,15 @@ class MenuViewModel extends HookWidget {
                 }
               }
               apiResponse.notifyListeners();
-              apiResponse.value.data = apiResponse.value.data;
               for (int i = 0; i < foodList.length; i++) {
                 if (foodList[i].menu_id == apiResponse.value.data[index].id) {
                   scrollController.animateTo(i * 120.toHeight,
+                      duration: Duration(seconds: 1), curve: Curves.ease);
+                }
+              }
+              for (int i = 0; i < apiResponse.value.data.length; i++) {
+                if (i==index) {
+                  horizontalScrollController.animateTo(i * 80.toWidth-i*10.toWidth,
                       duration: Duration(seconds: 1), curve: Curves.ease);
                 }
               }
