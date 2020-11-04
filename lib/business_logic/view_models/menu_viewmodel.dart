51aee95ca4a6f2f3c43817aea4634d0db9830e3b
konuleminova <konul.eminova39@gmail.com>
2020-11-04 13:13:30 +0400

    added resfresh

--- lib/business_logic/view_models/menu_viewmodel.dart
+++ lib/business_logic/view_models/menu_viewmodel.dart
@@ -14,14 +14,15 @@ class MenuViewModel extends HookWidget {
   final String id;
   final ScrollController scrollController;
   List<Food> foodList;
+  int index = 0;
 
   MenuViewModel({this.id, this.scrollController, this.foodList});
 
   @override
   Widget build(BuildContext context) {
-    ApiResponse<List<Menu>> apiResponse = useFetchMenu(id);
+    final ApiResponse<List<Menu>> apiResponse = useFetchMenu(id);
+
     useEffect(() {
-      //apiResponseValue.value
       print('Chnaged');
       return () {};
     }, [id]);
@@ -30,15 +31,12 @@ class MenuViewModel extends HookWidget {
       child: ListView.builder(
         itemBuilder: (BuildContext context, int index) {
           return InkWell(
-            child: MenuItem(
-              menu: apiResponse.data[index],
-            ),
+            child: MenuItem(menu: apiResponse.data[index]),
             onTap: () {
               apiResponse.data[index].selected=true;
               print('On click');
               for (int i = 0; i < foodList.length; i++) {
-                if (foodList[i].menu_id ==
-                    apiResponse.data[index].id) {
+                if (foodList[i].menu_id == apiResponse.data[index].id) {
                   scrollController.animateTo(i * 120.toHeight,
                       duration: Duration(seconds: 2), curve: Curves.ease);
                 }
