1871147800ee526564aadac1c299e1de5b5fe21d
konuleminova <konul.eminova39@gmail.com>
2020-11-27 10:26:45 +0400

    added custom menu drawer

--- lib/ui/views/food/widgets/menu_item.dart
+++ lib/ui/views/food/widgets/menu_item.dart
@@ -15,13 +15,19 @@ class MenuItem extends StatelessWidget {
       child: Container(
         height: 40.toHeight,
         decoration: BoxDecoration(
-            color: menu.selected ? ThemeColor().grey1.withOpacity(0.5) : Colors.white,
+            color: menu.selected
+                ? ThemeColor().yellowColor.withOpacity(0.8)
+                : Colors.white,
             borderRadius: BorderRadius.circular(30)),
         padding: EdgeInsets.all(8.toWidth),
         child: Center(
           child: Text(
             menu.name ?? 'no item',
-            style: TextStyle(color: ThemeColor().greyColor),
+            style: TextStyle(
+                color: menu.selected
+                    ? Colors.black87.withOpacity(0.8)
+                    : Colors.black45,
+                fontWeight: FontWeight.w400),
           ),
         ),
         margin: EdgeInsets.symmetric(horizontal: 8.toWidth),
