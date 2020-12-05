bf37b8a64e2087d3df498a5ae0341abbc7e2f18f
John Doe <konul.eminova39@gmail.com>
2020-12-05 22:17:59 +0400

    items count and total price done

--- lib/utils/map_utils/point_inpolygon.dart
+++ lib/utils/map_utils/point_inpolygon.dart
@@ -1,34 +1,19 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-bool isPointInPolygon(LatLng tap, List<LatLng> vertices) {
-  int intersectCount = 0;
-  for (int j = 0; j < vertices.length - 1; j++) {
-    if (rayCastIntersect(tap, vertices[j], vertices[j+1])) {
-      intersectCount++;
-    }
+bool isPointInPolygon (LatLng point, List<LatLng> vs) {
+  // ray-casting algorithm based on
+  // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
+
+  var x = point.latitude, y = point.longitude;
+
+  var inside = false;
+  for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+    var xi = vs[i].latitude, yi = vs[i].longitude;
+    var xj = vs[j].latitude, yj = vs[j].longitude;
+
+    var intersect = ((yi > y) != (yj > y))
+        && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+    if (intersect) inside = !inside;
   }
-
-  return ((intersectCount % 2) == 1); // odd = inside, even = outside;
-}
-
-bool rayCastIntersect(LatLng tap, LatLng vertA, LatLng vertB) {
-
-  double aY = vertA.latitude;
-  double bY = vertB.latitude;
-  double aX = vertA.longitude;
-  double bX = vertB.longitude;
-  double pY = tap.latitude;
-  double pX = tap.longitude;
-
-  if ((aY > pY && bY > pY) || (aY < pY && bY < pY)
-      || (aX < pX && bX < pX)) {
-    return false; // a and b can't both be above or below pt.y, and a or
-    // b must be east of pt.x
-  }
-
-  double m = (aY - bY) / (aX - bX); // Rise over run
-  double bee = (-aX) * m + aY; // y = mx + b
-  double x = (pY - bee) / m; // algebra is neat!
-
-  return x > pX;
+  return inside;
 }
