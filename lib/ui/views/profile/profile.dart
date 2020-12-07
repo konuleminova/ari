a1a61e01f3f44ea67bd73774674091d17334c5b7
konuleminova <konul.eminova39@gmail.com>
2020-12-07 11:40:59 +0400

    register page design

--- lib/ui/views/profile/profile.dart
+++ lib/ui/views/profile/profile.dart
@@ -1,4 +1,5 @@
 import 'package:ari/ui/views/profile/login.dart';
+import 'package:ari/ui/views/profile/register.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -7,6 +8,6 @@ class ProfileView extends HookWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return LoginView();
+    return RegisterView();
   }
 }
