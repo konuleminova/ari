efc3a5223f0607a761952a84c5bae51533679b61
konuleminova <konul.eminova39@gmail.com>
2020-12-16 14:38:43 +0400

    added change lang provider

--- lib/ui/views/food/widgets/food_item/food_item.dart
+++ lib/ui/views/food/widgets/food_item/food_item.dart
@@ -75,7 +75,7 @@ class FoodItem extends StatelessWidget {
                             ),
                             onTap: () {
                               item.selected = true;
-                              item.expanded=true;
+                              item.expanded = true;
                               addtoCartCallBack(item);
                             })
                       ],
@@ -90,17 +90,11 @@ class FoodItem extends StatelessWidget {
                 Expanded(
                   flex: 2,
                   child: ClipRRect(
-                    child: FadeInImage(
-                      image: NetworkImage(
-                        item.image ??
-                            'https://bees.az/___entcpanel/uploads/ef4534a27895456ef72f5acd7703ec9f_331778.png',
-                      ),
+                    child: Image.network(
+                      item.image,
                       height: 80.toHeight,
                       width: SizeConfig().screenWidth,
                       fit: BoxFit.cover,
-                      placeholder: AssetImage(
-                        'assets/images/no_image.png',
-                      ),
                     ),
                     borderRadius: BorderRadius.circular(4),
                   ),
