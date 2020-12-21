4ec05112c21c47deba0c5cc99e02ab68aa94f358
konuleminova <konul.eminova39@gmail.com>
2020-12-21 17:19:42 +0400

    register

--- lib/ui/views/food/widgets/food_item/food_item_expanded.dart
+++ lib/ui/views/food/widgets/food_item/food_item_expanded.dart
@@ -31,17 +31,9 @@ class FoodItemExpanded extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           ClipRRect(
-            child: FadeInImage(
-              image: NetworkImage(
-                food.image ??
-                    'https://bees.az/___entcpanel/uploads/ef4534a27895456ef72f5acd7703ec9f_331778.png',
-              ),
-              height: 150.toHeight,
-              width: SizeConfig().screenWidth,
-              fit: BoxFit.cover,
-              placeholder: AssetImage(
-                'assets/images/no_image.png',
-              ),
+            child: Image.network(
+              food.image,
+              // height: 150.toHeight,
             ),
             borderRadius: BorderRadius.circular(6),
           ),
@@ -67,7 +59,7 @@ class FoodItemExpanded extends StatelessWidget {
                         onPressed: () {
                           if (food.count == 1) {
                             food.selected = false;
-                            food.expanded=false;
+                            food.expanded = false;
                           } else {
                             food.selected = true;
                             food.count = food.count - 1;
@@ -139,7 +131,7 @@ class FoodItemExpanded extends StatelessWidget {
                       width: SizeConfig().screenWidth,
                       // padding: EdgeInsets.symmetric(vertical: 8.toWidth),
                       decoration: BoxDecoration(
-                          color: food.adds[index].type == 0
+                          color: food.adds[index].type == 0||food.adds[index].count>0
                               ? ThemeColor().yellowColor
                               : ThemeColor().grey1,
                           borderRadius: BorderRadius.circular(2)),
@@ -164,7 +156,7 @@ class FoodItemExpanded extends StatelessWidget {
                                                           .adds[index].count)) *
                                                   double.tryParse(
                                                       food.adds[index].price))
-                                              .toString() +
+                                              .toStringAsFixed(2) +
                                           '  ₼' ??
                                       '0',
                                   style: TextStyle(
