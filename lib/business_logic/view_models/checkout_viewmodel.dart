7e6bc2dbdc5f46d2c19c1d2f9e3f85db1ba5cc46
konuleminova <konul.eminova39@gmail.com>
2020-12-23 12:27:52 +0400

    register

--- lib/business_logic/view_models/checkout_viewmodel.dart
+++ lib/business_logic/view_models/checkout_viewmodel.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:ari/business_logic/models/checkout.dart';
 import 'package:ari/business_logic/routes/route_navigation.dart';
 import 'package:ari/services/api_helper/api_response.dart';
@@ -11,9 +13,12 @@ import 'package:ari/ui/provider/checkout/checkout_action.dart';
 import 'package:ari/ui/provider/checkout/checkout_state.dart';
 import 'package:ari/ui/provider_components/counter_state.dart';
 import 'package:ari/ui/views/checkout/checkout.dart';
+import 'package:ari/utils/sharedpref_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoder/geocoder.dart';
 
 class CheckoutViewModel extends HookWidget {
   RouteArguments<Checkout> checkout;
@@ -36,6 +41,28 @@ class CheckoutViewModel extends HookWidget {
       }
       return () {};
     }, [apiResponse2]);
+
+    useEffect(() {
+      apiResponse2.status=Status.Loading;
+      if (Platform.isAndroid) {
+        Geolocator.checkPermission().then((value) {
+          Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+              .then((value) {
+            SpUtil.putString(SpUtil.lat, value.latitude.toString());
+            SpUtil.putString(SpUtil.lng, value.longitude.toString());
+            Geocoder.local
+                .findAddressesFromCoordinates(
+                    Coordinates(value.latitude, value.longitude))
+                .then((valuex) {
+              SpUtil.putString(SpUtil.address, valuex.first.addressLine ?? '');
+              print("MY ADDRESS ${valuex.first.addressLine}");
+              apiResponse2.status=Status.Done;
+            });
+          });
+        });
+      }
+      return () {};
+    }, []);
 
     return CustomErrorHandler(
       statuses: [apiResponse.status],
