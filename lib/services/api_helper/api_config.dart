b1dd09c7323f9ef36a4c6f22d6940b1eebfdee01
konuleminova <konul.eminova39@gmail.com>
2020-12-24 14:06:32 +0400

    languages

--- lib/services/api_helper/api_config.dart
+++ lib/services/api_helper/api_config.dart
@@ -6,7 +6,7 @@ class ApiConfig {
   String FOOD_URl(String id) => '${BASE_URl}food&restourant_id=${id}&gr=1';
 
   String RESTOURANT_URl(String id, String lang,String token) =>
-      '${BASE_URl}mainpage&text=${id}&${token}';
+      '${BASE_URl}mainpage&text=${id}&token=${token}';
 
   String MENU_URL(String id) => '${BASE_URl}menu&restourant_id=${id}';
 
