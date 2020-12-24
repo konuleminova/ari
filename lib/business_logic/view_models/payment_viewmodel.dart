9c647a47d9024da52c4ad508e1f4141d36023e5f
konuleminova <konul.eminova39@gmail.com>
2020-12-24 14:10:38 +0400

    languages

--- lib/business_logic/view_models/payment_viewmodel.dart
+++ lib/business_logic/view_models/payment_viewmodel.dart
@@ -12,7 +12,6 @@ import 'package:ari/services/api_helper/api_response.dart';
 import 'package:ari/services/hooks/use_callback.dart';
 import 'package:ari/services/services/payment_service.dart';
 import 'package:ari/ui/common_widgets/loading.dart';
-import 'package:ari/ui/views/init.dart';
 import 'package:ari/utils/theme_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
