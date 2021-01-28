ae69e8e34800c104531faf951d61febd6587b79f
konuleminova <konul.eminova39@gmail.com>
2021-01-28 20:03:06 +0400

    part4

--- lib/business_logic/models/food.dart
+++ lib/business_logic/models/food.dart
@@ -42,7 +42,7 @@ class Food {
     menu_id = json['menu_id'];
     restourant_id = json['restourant_id'];
     adds = listAddsFromJsom(json['adds']);
-    apiCount=json['count'];
+    apiCount=int.parse(json['count'].toString());
   }
 
   @override
